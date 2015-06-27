315ddccdc0fa577fe6907782f58e572fb9eb8e5c
Ritik Mishra <ritik.mishra314@gmail.com>
2015-06-26 23:36:15 -0500

    Made level look like level. Gingerman falls through floor, though

--- Platformer For Vinny.py
+++ Platformer For Vinny.py
@@ -7,58 +7,58 @@ bgcolor = 135, 206, 235
 running = 1
 
 class Cloud:
-	""" 'And God said 'Let there be clouds!' ' """
-	def __init__(self):
-		global height, width, screen
-		self.img = pygame.image.load("8bit_cloud.png")
-		self.size = self.img.get_size()
-		self.screen = screen
-		self.altitude = random.randint(0, (height/3)/self.size[1]) * self.size[1] 
-		self.position = random.randint(0, width)
-		
-	def move(self):
-		self.position += 0.5
-		
-		self.screen.blit(self.img, (self.position,self.altitude))
-		if self.position == 480+self.size[0]:
-			self.altitude = random.randint(0, (height/2)/self.size[1]) * self.size[1] 
-			self.position = 0-self.size[0]
-			
+    """ 'And God said 'Let there be clouds!' ' """
+    def __init__(self):
+        global height, width, screen
+        self.img = pygame.image.load("8bit_cloud.png")
+        self.size = self.img.get_size()
+        self.screen = screen
+        self.altitude = random.randint(0, (height/3)/self.size[1]) * self.size[1] 
+        self.position = random.randint(0, width)
+        
+    def move(self):
+        self.position += 0.5
+        
+        self.screen.blit(self.img, (self.position,self.altitude))
+        if self.position == 480+self.size[0]:
+            self.altitude = random.randint(0, (height/2)/self.size[1]) * self.size[1] 
+            self.position = 0-self.size[0]
+            
 class StartMenuItem(pygame.sprite.Sprite):
-	""" God: 'Dammit how do I start this man-damn game?'
-		Jeffery: 'Just click that little sprite part of the StartMenuItem Class' """
-	def __init__(self, img_location, posY, centered=True, posX=None):
-		global height, width, screen
-		super(StartMenuItem).__init__(StartMenuItem)
-		self.img = pygame.image.load(str(img_location))
-		
+    """ God: 'Dammit how do I start this man-damn game?'
+        Jeffery: 'Just click that little sprite part of the StartMenuItem Class' """
+    def __init__(self, img_location, posY, centered=True, posX=None):
+        global height, width, screen
+        super(StartMenuItem).__init__(StartMenuItem)
+        self.img = pygame.image.load(str(img_location))
+        
 
-		self.size = self.img.get_size()
-		self.screen = screen
-		self.altitude = int(posY)
-		if centered:
-			self.posX = (width / 2) - (self.size[0] / 2)
-		else:
-			self.posX = posX
-			
-		self.rect = pygame.Rect((self.posX, self.altitude), self.size)
-	def display(self, buttontype='Text'):
-		self.screen.blit(self.img, (self.posX, self.altitude))
-		
-			
-	def clicked(self, buttontype):
-		global running, width, height
-		if buttontype == 'quit':
-			running = 0
-		elif buttontype == 'start':
-			level.main(width, height)
+        self.size = self.img.get_size()
+        self.screen = screen
+        self.altitude = int(posY)
+        if centered:
+            self.posX = (width / 2) - (self.size[0] / 2)
+        else:
+            self.posX = posX
+            
+        self.rect = pygame.Rect((self.posX, self.altitude), self.size)
+    def display(self, buttontype='Text'):
+        self.screen.blit(self.img, (self.posX, self.altitude))
+        
+            
+    def clicked(self, buttontype):
+        global running, width, height
+        if buttontype == 'quit':
+            running = 0
+        elif buttontype == 'start':
+            level.main(width, height)
 class Character:
-	def __init__(self):
-		global height, width, screen
-		self.posX = width/2
-		self.posY = height/2
-		
-		
+    def __init__(self):
+        global height, width, screen
+        self.posX = width/2
+        self.posY = height/2
+        
+        
 cloud1 = Cloud()
 cloud2 = Cloud()
 cloud3 = Cloud()
@@ -69,24 +69,24 @@ stopgame = StartMenuItem("quit.png", 363)
 
 pygame.display.set_caption('Psuedo-nomed')
 while running:
-	mouse_pos = pygame.mouse.get_pos()
-	event = pygame.event.poll()
-	if event.type == pygame.QUIT:
-		running = 0
-	
-	if  pygame.mouse.get_pressed()[0] and start.rect.collidepoint(mouse_pos):
-		start.clicked('start')
-		
-	elif pygame.mouse.get_pressed()[0] and stopgame.rect.collidepoint(mouse_pos):
-		stopgame.clicked('quit')
+    mouse_pos = pygame.mouse.get_pos()
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        running = 0
+    
+    if  pygame.mouse.get_pressed()[0] and start.rect.collidepoint(mouse_pos):
+        start.clicked('start')
+        
+    elif pygame.mouse.get_pressed()[0] and stopgame.rect.collidepoint(mouse_pos):
+        stopgame.clicked('quit')
 
-	screen.fill(bgcolor)
-	cloud1.move()
-	cloud2.move()
-	cloud3.move()
-	
-	title.display()
-	start.display()
-	stopgame.display()
-	
-	pygame.display.flip()
+    screen.fill(bgcolor)
+    cloud1.move()
+    cloud2.move()
+    cloud3.move()
+    
+    title.display()
+    start.display()
+    stopgame.display()
+    
+    pygame.display.flip()
--- level.py
+++ level.py
@@ -9,108 +9,102 @@ def main(width, height):
     window = pygame.display.set_mode(dimensions)
     no_heli_y = []
     no_heli_x = []
-	
-		
+    
+        
     #helps with making the close button in the corner work
     running = 1
-    class Helicopter:
+    class Level(pygame.sprite.Sprite):
+        def __init__(self, screen, width, height, img):
+            super(Level).__init__(Level)
+            self.screen = screen
+            self.img = pygame.image.load(img)
+            self.size = self.img.get_size()
+            self.height = self.size[0]
+            self.width = self.size[1]
+            self.posX=0
+            self.posY=650
+            self.rect = pygame.Rect((self.posX, self.posY), self.size)
+        def display(self):
+            self.screen.blit(self.img, (self.posX, self.posY))
+            print self.rect
+    class Character(pygame.sprite.Sprite):
         """Contains all the functions and variables that only the helicopter needs"""
-        def __init__(self, screen):
+        def __init__(self, screen, width, height):
+            super(Level).__init__(Level)
             """Creates all the essential variables"""
-            self.change = 1
-            self.y = 50
-            self.x = 25
-            self.img = pygame.image.load("Ginger_forward.png")
-            self.size = self.img.get_size()
-            self.click = 0
+            self.forward_img = pygame.image.load("Ginger_forward.png")
+            self.right_img = pygame.image.load("Ginger_right.png")
+            self.left_img = pygame.image.load("Ginger_left.png")
+            
+            self.forward_size = self.forward_img.get_size()
+            self.right_size = self.right_img.get_size()
+            self.left_size = self.left_img.get_size()
+            
+            self.size = self.forward_size
+            self.img = self.forward_img
+            
             self.screen = screen
+            self.posY = height/2
+            self.posX = width/2
             
-        def move(self):
-            """Moves the helicopter if computer detects left-click"""
-            
-            self.click = pygame.mouse.get_pressed()[0]
-            if self.click == 1:
-                self.change = -1
-            else:
-                self.change = 1
-            self.y += self.change
-        def blit(self):
-            """Displays the helicopter"""
-            self.corners = [(self.x, self.y), (self.x, self.y + self.size[1]), (self.x + self.size[0], self.y), (self.x + self.size[0], self.y + self.size[1])] 
-            self.screen.blit(self.img, (self.x, self.y))
+            self.rect = pygame.Rect((self.posX, self.posY), self.size)
+   
         
+        def move(self, ground_rect, direction='forward'):
+            if not self.rect.colliderect(ground_rect):
+                self.posY +=1
+                self.size = self.forward_size
+                self.img = self.forward_img
+                self.screen.blit(self.img, (self.posX, self.posY))
+        
+            if direction == 'forward':
+                self.size = self.forward_size
+                self.img = self.forward_img
+                self.screen.blit(self.img, (self.posX, self.posY))
+            elif direction == 'left':
+                self.size = self.left_size
+                self.img = self.left_img
+                self.posX -= 1
+                self.screen.blit(self.img, (self.posX, self.posY))
+               
+            elif direction == 'right':
+               self.size = self.right_size
+               self.img = self.right_img
+               self.posX += 1
+               self.screen.blit(self.img, (self.posX, self.posY))
 
-    class Obstacle:
-        """Contains all the functions and variables specific to the boxes"""
-        def __init__(self, screen):
-            self.change = 4
-            self.y = random.randrange(0, 560)
-            self.x = 999
-            self.img = pygame.image.load("Ginger_left.png")
-            self.size = self.img.get_size()
-            self.screen = screen
-            self.perim = []
-        def move(self):
-            """Moves the murderous obstacle towards its destination's x point"""
-            self.x -= self.change
-            if self.x <= 1:
-                self.x = 999
-                self.y = random.randrange(0, 560)
-        def blit(self):
-            """Makes the user able to see and thus evade the obstacle"""
-            #The next two lines are for collision detection
-            self.corners = [(self.x, self.y), (self.x, self.y + self.size[1]), (self.x + self.size[0], self.y + self.size[1]), (self.x + self.size[0], self.y)] 
-            
-            self.screen.blit(self.img, (self.x, self.y))
+
            
         
 
     #The user's helicopter
-    heli = Helicopter(window)
-    obs=[Obstacle(window), Obstacle(window), Obstacle(window)]
-    
+    level = Level(window, width, height, "Level 1.png")
+    gingerman = Character(window, width, height)
 
         
         
     score = 0
     #gameloop
     while running:
-         #makes the close button functional
-        event = pygame.event.poll()
-        if event.type == pygame.QUIT:
-            running = 0
+        #makes the close button functional
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    gingerman.move(level.rect, 'left')
+                if event.key == pygame.K_RIGHT:
+                    gingerman.move(level.rect, 'right')
+        #makes the background blue 
+        window.fill((135, 206, 235))
+        level.display()
+        window.blit(level.img, (level.posX, level.posY))
+        gingerman.move(level.rect)
         
-        #makes the background black 
-        window.fill((0, 0, 0))
-        
-        
-        #moves the helicopter
-        heli.move()
-        obs[0].move()
-        obs[1].move()
-        obs[2].move()
-        
-       
-        
-        #shows the helicopter
-        heli.blit()
-        obs[0].blit()
-        obs[1].blit()
-        obs[2].blit()
 
-        if heli.y <= 1 or heli.y >= 500:
-            break
-        
-        for corner in heli.corners:
-            if corner[1] in range(obs[0].y, obs[0].y + 40) and corner[0] in range(obs[0].x, obs[0].x + 64):
-                exit()
-            
-            elif corner[1] in range(obs[1].y, obs[1].y + 40) and corner[0] in range(obs[1].x, obs[1].x + 64):
-                exit()
-            
-            elif corner[1] in range(obs[2].y, obs[2].y + 40) and corner[0] in range(obs[2].x, obs[2].x + 64):
-                exit()  
+      
         pygame.display.flip()
-    print("You lose!")
+
 if __name__ == '__main__':
-    print "This file is meant to be a part of Psuedo-Nomed"
+    main(1204, 720)
