9f3373d1639ff034df11672d3b9fb39952da5203
Ritik Mishra <ritik.mishra314@gmail.com>
2015-07-05 21:55:35 -0500

    added comments to level.py

--- level.py
+++ level.py
@@ -4,17 +4,20 @@ def main(width, height):
 	import os
 	import random
 	import time
+	
 	#create the screen
 	dimensions = (width, height)
 	window = pygame.display.set_mode(dimensions)
-	no_heli_y = []
-	no_heli_x = []
 	
-		
 	#helps with making the close button in the corner work
 	running = 1
+	
 	class PowerUp(pygame.sprite.Sprite):
+		"""We'll want powerups, or the game will be less fun"""
+		
 		def __init__(self, screen, width, height, image, version, posX = False, posY = False):
+			"""Initializes essential variables, loads image, places image, etc..."""
+		
 			super(Level).__init__(Level)
 			self.version = version
 			self.screen = screen
@@ -40,17 +43,22 @@ def main(width, height):
 			
 		
 		def display(self):	
+			"""Shows image"""
 			self.screen.blit(self.image, (self.posX, self.posY))
 		def speedup(self, character_obj):
+			"""Applies powerup. In this case, a speedup."""
 			if pygame.sprite.collide_rect(self, character_obj):
 				character_obj.posX += self.size[0]
 
 		def __str__(self):
+			"""If we want to print the function, the print statement will output important info"""
 			return "Type: " +  str(self.version) + "\n Location: " +  str(self.rect)
 			
 			
 	class Level(pygame.sprite.Sprite):
+		"""The ground"""
 		def __init__(self, screen, width, height, image):
+			"""Makes essential variables, loads the image, and sets the initial scroll"""
 			super(Level).__init__(Level)
 			self.screen = screen
 			self.image = pygame.image.load(image)
@@ -65,19 +73,21 @@ def main(width, height):
 			self.mask = pygame.mask.from_surface(self.image)
 			
 		def display(self):
+			"""Shows the ground"""
 			self.screen.blit(self.image, (self.posX, self.posY))
 			self.rect = pygame.Rect((self.posX, self.posY), self.size)
 		def scroll(self, direction,distance):
-			
+			"""If the player is far enough to either side, we want to move the ground so the player can see the new ground"""
 			if direction == 'right':
 				self.posX -= self.screen_width + distance
 			elif direction == 'left':
 				self.posX += self.screen_width - distance
+				
 	class Character(pygame.sprite.Sprite):
-		"""Contains all the functions and variables that only the helicopter needs"""
+		"""Contains all the functions and variables that only the player needs"""
 		def __init__(self, screen, width, height):
 			super(Level).__init__(Level)
-			"""Creates all the essential variables"""
+			"""Creates all the essential variables. Also loads image, which is very important"""
 			self.forward_img = pygame.image.load("Ginger_forward.png")
 			self.right_img = pygame.image.load("Ginger_right.png")
 			self.left_img = pygame.image.load("Ginger_left.png")
@@ -103,7 +113,7 @@ def main(width, height):
    
 		
 		def move(self, ground_rect, direction='forward'):
-			
+			"""We want to be able to move """
 			if not pygame.sprite.collide_rect(self, ground_rect):
 				if not pygame.sprite.collide_mask(self, ground_rect):
 					self.posY +=1
@@ -161,21 +171,32 @@ def main(width, height):
 
 
 
-	#The user's helicopter
+	#The ground
 	level = Level(window, width, height, "Level 1a.png")
+	
+	#The character, who's sprite is a gingerbread man
 	gingerman = Character(window, width, height)
+	
+	#The powerup
 	speedup = PowerUp(window, width, height, 'arrow.png', 'Launcher', False, 125)
 	
-		
-	score = 0
-	#gameloop
+	#If the arrow keys are held down, we want the character to continously move. This helps with that.	
 	pygame.key.set_repeat(100, 50)
+	
+	#Gameloop, executes all the actions
 	while running:
-		#makes the close button functional
+		
+		#Gets a list of what's happening
 		events = pygame.event.get()
+		
+		#Reads the list of what's happening
 		for event in events:
+			
+			#Exits program if red X cliked
 			if event.type == pygame.QUIT:
 				running = 0
+			
+			#Moves the character if the keys are pressed
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
 					gingerman.move(level, 'left')
@@ -186,13 +207,21 @@ def main(width, height):
 
 		#makes the background blue 
 		window.fill((135, 206, 235))
+		
+		#Shows the level
 		level.display()
-		#speedup.display()
+		
+		#speedup.display() |This is commented because the speedup is glitchy
+		
 		
 	
 		
-		#speedup.speedup(gingerman)
+		#speedup.speedup(gingerman) |This is commented because the speedup is glitchy
+		
+		#Makes sure physics applies
 		gingerman.move(level)
+		
+		#Shows everything
 		pygame.display.flip()
 
 if __name__ == '__main__':
